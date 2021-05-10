94bfe1c2189794fabb32371bdcf23fd051545a66
albina.zemskova <albina@buildmidwestern.com>
2021-05-10 14:32:31 +0300

    Resize padding in the meet the dev section

--- src/sections/landing1/Content2.js
+++ src/sections/landing1/Content2.js
@@ -110,7 +110,7 @@ const Content2 = () => (
               data-aos-once="true"
             >
               <div className="content-text pl-lg--50">
-                <div className="section-title">
+                <div className="section-title pl-lg-5">
                   <Title>meet the developer</Title>
                   <Text mb={4}>
                     Drew Roberts grew up on a farm in Kentucky and began
