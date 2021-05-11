101fb6e6317c65844e77aec2d1635fa8f3646932
albina.zemskova <albina@buildmidwestern.com>
2021-05-11 13:47:20 +0300

    Adjust left and right padding on sections

--- src/sections/landing1/Content1Version2.js
+++ src/sections/landing1/Content1Version2.js
@@ -5,14 +5,15 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Title, Button, Section, Box, Text } from "../../components/Core";
 import { device, theme } from "../../utils";
 import ape2Image from "../../assets/image/png/ape-2.png";
-import imgL1FeatureCurve from "../../assets/image/svg/swish-bottom-black.svg";
+import imgL1FeatureCurve from "../../assets/image/svg/swish-bottom.svg";
 import cfga from "../../assets/image/svg/cfga.svg";
 
 const ShapeBottmRight = styled(Box)`
   position: absolute;
-  bottom: -3px;
+  bottom: 0;
   left: -5px;
   width: calc(100vw + 10px);
+  background-color: ${({ theme }) => theme.colors.dark};
   img {
     min-width: 100%;
   }
